fc9ac2cd3a77cd6a97131d1347d83d582c869f91
Sachin <Sachinsharma0893@gmail.com>
2020-06-16 20:42:11 +0530

    add Authentication and User Signup Support

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:notice] = "Your account Information was successfully updated"
+          redirect_to articles_path
+        else
+          render 'edit'
+        end
+      end
+
+    def create
+        @user = User.new(user_params)
+          if @user.save
+            flash[:notice] = "Welcome to Alpha Blog #{@user.username}. You are successfully Signed up"
+            redirect_to articles_path
+          else
+            render :new
+          end
+     end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+      end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :users, except: [:new]
   root 'pages#home'
   get 'about', to:'pages#about'
+  get 'signup', to: 'users#new'
 end
